51f5df0051e808515d6db56325f5e1a9fb788377
Franco <francoherrera_8@hotmail.com.ar>
2023-05-29 19:27:59 -0300

    primer comentario

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <div id="banner">
+        <img src="./img/ocean-g177104987_1920.jpg" alt=""></img>
+        <div >
+            <h1 >Franco Herrera Ruiz</h1>
+            <h2 ><em>Programador Web FullStack</em></h2>
+            <a class="btn btn-success btn-xl boton" href="#portfolio">Proyectos</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/components/Projectos/Projectos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projectos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Projectos
