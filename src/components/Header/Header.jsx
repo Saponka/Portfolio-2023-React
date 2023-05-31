9523e0461946dc37b6a46466d97a61340e73c90d
Franco <francoherrera_8@hotmail.com.ar>
2023-05-30 22:13:09 -0300

    header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import perfil from "../../assets/Img/perfil.png";
 
 const Header = () => {
@@ -6,37 +6,42 @@ const Header = () => {
     <>
       <div id="banner">
         {/* <img src="./img/ocean-g177104987_1920.jpg" alt=""></img> */}
-        <div className='divNombre'>
-            <h1 className='nombre' >Franco Herrera Ruiz</h1>
-            <h2 className='subtitulo' ><em>Programador Web FullStack</em></h2>
-            {/* <a class="btn btn-success btn-xl boton" href="#portfolio">Proyectos</a> */}
-            {/* <!-- Acerca de mi --> */}
-            <section class="content-section  text-dark text-center  p-5" id="AcercaDeMi" >
-        <div class="container px-4 px-lg-5  text-center">
-            <div id="foto">
-                <img src={perfil} alt=""></img>
-            </div><br/>
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div >
-                    
-                    <p class="lead mb-5 descripcion" >
-                    Mi nombre es Franco Herrera Ruiz naci en Río Cuarto, Córdoba, Argentina.<br/>
-                    Actualmente soy Desarrollador FullStack y Chef Profesional.<br/>
-                    Despues de 10 años de ejercer en gastronomia me anime a dar un cambio en mi vida que me llevo a elegir la programacion como mi nueva vocacion.<br/>
-                    Realice una Diplomatura en la UTN de FullStack developer y recientemente me gradue del Bootcamp de Soy Henry en FullStack.<br/>
-                    Mi meta es continuar en mi formacion como programador, para acceder y manejar diferentes lenguajes.<br/>
-                    Considero que siempre hay algo para hacer y aprender ante un desafio.
-                    </p>
-                    <a class="button boton" href="#services">Experiencia</a>
-                </div>
-            </div>
+        <div className="divNombre">
+          <h1 className="nombre">Franco Herrera Ruiz</h1>
+          <h2>
+            <em className="subtitulo">Programador Web FullStack</em>
+          </h2>
         </div>
-      </section>
+
+        <div id="foto" style={{ display: "flex" }}>
+          <img src={perfil} alt=""></img>
         </div>
+        <br />
       </div>
-      
+      {/* <!-- Acerca de mi --> */}
+      <section
+        className="content-section  text-dark text-center  p-5"
+        id="AcercaDeMi"
+      >
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+          <div>
+            <h2 className="lead mb-5 descripcion">
+              Soy Franco Herrera Ruiz de Río Cuarto, Córdoba, Argentina.
+              <br />
+              Actualmente soy Desarrollador Web FullStack y Chef Profesional.
+              <br />
+              Mi meta es continuar en mi formacion como programador, para
+              acceder y manejar diferentes lenguajes.
+              <br />
+              Considero que siempre hay algo para hacer y aprender ante un
+              desafio.
+            </h2>
+          </div>
+        </div>
+        {/*  <a className="button boton" href="#services">Experiencia</a> */}
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default Header;
