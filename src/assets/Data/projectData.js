f30708c1399dceaaf5591ac6ac6a1ae90766aa23
Franco <francoherrera_8@hotmail.com.ar>
2023-05-31 19:36:24 -0300

    data array objets

---
+++ src/assets/Data/projectData.js
@@ -0,0 +1,67 @@
+
+let itemData = {
+
+    items:[
+    {
+        id:1,
+        name: "Weather Channel",
+        img:require('../Img/proyectos/weatherChannel.png'),
+        description:"Aplicacion del clima,donde se consume una api con Ajax"
+    },
+    {
+        id:2,
+        name: "Lista de Tareas",
+        img:require("../Img/proyectos/ListaDeTareas.png"),
+        description:"To Do List,para crear, editar y borrar(D.O.M)"
+    },
+    {
+        id:3,
+        name: "A-Venture",
+        img:require("../Img/proyectos/a-venture.png"),
+        description:"Elige tu propia aventura y toma las decisiones"
+    },
+    {
+        id:4,
+        name: "Color Game",
+        img:require("../Img/proyectos/colorete.png"),
+        description:"Juego de adivinar el color RGB (D.O.M)"
+    },
+    {
+        id:5,
+        name: "Typer 3000",
+        img:require("../Img/proyectos/typer3000.png"),
+        description:"Velocidad,Tiempo,Palabras Este Juego es para vos"
+    },
+    {
+        id:6,
+        name: "Resto Theme",
+        img:require("../Img/proyectos/resto.png"),
+        description:"Template para negocio gastronomico"
+    },
+    {
+        id:7,
+        name: "Comix Zoom",
+        img:require("../Img/proyectos/comixZoom.png"),
+        description:"App de fandome de comics"
+    },
+    {
+        id:8,
+        name: "Video Games API",
+        img:require("../Img/proyectos/videoGames.png"),
+        description:"API REST de Video Juegos"
+    },
+    {
+        id:9,
+        name: "Bonzai Tek ",
+        img:require("../Img/proyectos/bonzayTek.png"),
+        description:"Gestion de datos multiples(MERN stack)"
+    },
+    {
+        id:10,
+        name: "Vino Rojo Bodegon",
+        img:require("../Img/proyectos/vinoRojo.png"),
+        description:"E-commerce gastronomico (MERN stack)"
+    },
+]
+};
+export default itemData;
