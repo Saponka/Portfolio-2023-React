73c55cd8aba04298faa002c4cd10977ac6dc79be
Franco <francoherrera_8@hotmail.com.ar>
2023-05-31 19:43:13 -0300

    navbar qr and download cv

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,147 +1,216 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 
-
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  '& .MuiSwitch-switchBase': {
-    margin: 1,
-    padding: 0,
-    transform: 'translateX(6px)',
-    '&.Mui-checked': {
-      color: '#fff',
-      transform: 'translateX(22px)',
-      '& .MuiSwitch-thumb:before': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff',
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-      },
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-      },
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-    width: 32,
-    height: 32,
-    '&:before': {
-      content: "''",
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      left: 0,
-      top: 0,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        '#fff',
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-    },
-  },
-  '& .MuiSwitch-track': {
-    opacity: 1,
-    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-    borderRadius: 20 / 2,
-  },
-}));
-
+//imports cv
+import resume from "../../assets/Data/FrancoResume.pdf";
+import qr from "../../assets/Img/FrancoResume.png"
+//qr code antdesign
+import { QRCode, Space, theme } from 'antd';
+const { useToken } = theme;
 
 const NavBar = () => {
+
+  //const { token } = useToken();
+  //const size = 84;
+
   return (
     <>
-    
-      <nav className="navbar is-transparent is-fixed-top">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="">
-      <img src="https://bulma.io/images/bulma-logo.png" alt="" width="112" height="28"></img>
-    </a>
-    <div className="navbar-burger" data-target="navbarExampleTransparentExample">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </div>
+      {/* <!-- Navigation--> */}
+      <nav className="navbar navbar-dark bg-dark  fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="#"></a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="offcanvas offcanvas-end "
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              {/* <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Franco Herrera Ruiz</h5>  */}
 
-  <div id="navbarExampleTransparentExample" className="navbar-menu">
-    <div className="navbar-start " id='navItems'>
-      
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link bd-tw-button button" style={{textDecoration:"none"}}>
-          Projectos
-        </a>
-        <div className="navbar-dropdown is-boxed">
-          <a className="navbar-item" href="https://bulma.io/documentation/overview/start/" style={{textDecoration: "none"}}>
-            Vino Rojo Bodegon
-          </a>
-          <a className="navbar-item" href="https://bulma.io/documentation/overview/modifiers/" style={{textDecoration: "none"}}>
-            Games API
-          </a>
-          <a className="navbar-item" href="https://bulma.io/documentation/columns/basics/" style={{textDecoration: "none"}}>
-            Color Game
-          </a>
-          <a className="navbar-item" href="https://bulma.io/documentation/layout/container/" style={{textDecoration: "none"}}>
-            Typer 3000
-          </a>
-          <a className="navbar-item" href="https://bulma.io/documentation/form/general/" style={{textDecoration: "none"}}>
-            Weather API
-          </a>
-          <hr className="navbar-divider" />
-          <a className="navbar-item" href="https://bulma.io/documentation/elements/box/" style={{textDecoration: "none"}}>
-          Lista de Tareas
-          </a>
-          <a className="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/" style={{textDecoration: "none"}}>
-            Comix Zoom
-          </a>
-          <a className="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/" style={{textDecoration: "none"}}>
-            Bonzai ZenTek
-          </a>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end  pe-3">
+                <li className="nav-item">
+                  <div id="capa">
+                    <div className="controls">
+                      <button className="ligth">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-sun"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                        </svg>
+                      </button>
+                      <button
+                        className="dark"
+                        style={{ backgroundColor: "black", color: "white" }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-moon"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
+                        </svg>
+                      </button>
+                      <span> Dark Mode</span>
+                    </div>
+                  </div>
+                </li>
+                <br />
+
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#portfolio"
+                    id="offcanvasNavbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Proyectos
+                  </a>
+                  <ul
+                    className="dropdown-menu dropdown-menu-dark"
+                    aria-labelledby="offcanvasNavbarDropdown"
+                  >
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://saponka.github.io/ColorGame/"
+                      >
+                        Color Game
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://saponka.github.io/weather-channel/"
+                      >
+                        Weather Channel
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://comixzoom.herokuapp.com/"
+                      >
+                        ComixZoom
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://saponka.github.io/ListaTareas/"
+                      >
+                        Lista de Tareas
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://typer3000.vercel.app/"
+                      >
+                        Typer 3000
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://video-games-delta.vercel.app/"
+                      >
+                        Video Games
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://vino-rojo-bodegon.vercel.app/"
+                      >
+                        Vino Rojo
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://final-react-front.vercel.app/"
+                      >
+                        Bonzai Tek
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Otras
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <div>
+                <ul>
+                  <li className="nav-item">
+                    <a
+                      download="FrancoResume.pdf"
+                      href={resume}
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      <button type="button" className="btn btn-outline-info">
+                        <span className="icon">
+                          <i className="fas fa-download"></i>
+                        </span>
+                        Download CV .pdf
+                      </button>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <p> or Download CV with QR</p>
+                    <img src={qr} alt="" width={110} />
+                {/* <Space>
+                  <QRCode
+                    value="https://github.com/Saponka"
+                    color={token.colorInfoText}
+                    bgColor={token.colorBgLayout}
+                    size={size}
+                    icon=''
+                  />
+                </Space> */}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-       
-      </div>
-      <FormGroup>
-      <FormControlLabel
-        control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked className='ligth' />}
-       /*  label="Dark Mode" */
-      />
-    </FormGroup>
-    </div>
-     
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="field is-grouped">
-          <p className="control">
-            <a className="bd-tw-button button" data-social-network="Github" data-social-action="github" data-social-target="https://github.com" target="_blank" href="https://github.com/Saponka" style={{textDecoration: "none"}}>
-              <span className="icon">
-              <i className="fa-brands fa-github fa-xl"   ></i>
-            
-              </span>
-              <span>
-                GitHub
-              </span>
-            </a>
-          </p>
-          <p className="control">
-            <a className="button is-danger" href="https://github.com/jgthms/bulma/releases/download/0.9.4/bulma-0.9.4.zip" style={{textDecoration: "none"}}>
-              <span className="icon">
-                <i className="fas fa-download"></i>
-              </span>
-              <span>Download CV</span>
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+      </nav>
     </>
-  )
+  );
 }
 
 export default NavBar
