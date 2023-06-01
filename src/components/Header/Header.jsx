588ef1b970f68f836d7ad57ed6193b2335d91d75
Franco <francoherrera_8@hotmail.com.ar>
2023-05-31 22:04:54 -0300

    header listo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,24 +5,18 @@ const Header = () => {
   return (
     <>
       <div id="banner">
-        {/* <img src="./img/ocean-g177104987_1920.jpg" alt=""></img> */}
-        <div className="divNombre">
+        <div className="divNombre" id="AcercaDeMi">
           <h1 className="nombre">Franco Herrera Ruiz</h1>
           <h2>
             <em className="subtitulo">Programador Web FullStack</em>
           </h2>
         </div>
-
         <div id="foto" style={{ display: "flex" }}>
           <img src={perfil} alt=""></img>
         </div>
         <br />
       </div>
-      {/* <!-- Acerca de mi --> */}
-      <section
-        className="content-section  text-dark text-center  p-5"
-        id="AcercaDeMi"
-      >
+      <section className="content-section text-dark text-center  p-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div>
             <h2 className="lead mb-5 descripcion">
@@ -30,15 +24,14 @@ const Header = () => {
               <br />
               Actualmente soy Desarrollador Web FullStack y Chef Profesional.
               <br />
-              Mi meta es continuar en mi formacion como programador, para
-              acceder y manejar diferentes lenguajes.
+              Mi meta es continuar en mi formacion como programador,
+              <br /> para acceder y manejar diferentes lenguajes.
               <br />
               Considero que siempre hay algo para hacer y aprender ante un
               desafio.
             </h2>
           </div>
         </div>
-        {/*  <a className="button boton" href="#services">Experiencia</a> */}
       </section>
     </>
   );
