9875dccf7a388ec20213f4fe676e8f177ba54a0d
Franco <francoherrera_8@hotmail.com.ar>
2023-05-31 22:04:30 -0300

    contacto listo

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Contacto = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <section className="content-section  text-white p-5">
+        <div className="container px-4 px-lg-5 text-center">
+            <h2 id='contacto' className="mb-4" >Gracias por visitar mi portfolio...</h2>
+           
+            <a className="btn btn-lg btn-outline-info boton2" href="#AcercaDeMi">Acerca de mi!</a>
+        </div>
+    </section>
+    </>
   )
-}
+};
 
-export default Contacto
+export default Contacto;
