8178c764ae888e54d623e573a690ce9fe209fcb6
Franco <francoherrera_8@hotmail.com.ar>
2023-06-11 23:55:09 -0300

    footer component icons y date

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./footer.css";
+
 
 const Footer = () => {
+
+  const date = new Date();
+
   return (
     <>
       <footer className="footer text-center">
         <div className="container px-4 px-lg-5">
-            <ul className="list-inline mb-5">
+            {<ul className="list-inline mb-5">
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white mr-3"target="blank" href="https://www.instagram.com/?hl=es-la"><i className="icon-social-instagram"></i></a>
+                    <a className="social-link rounded-circle text-white mr-3"target="blank" href="https://www.instagram.com/?hl=es-la"> <i class="fa-brands fa-instagram fa-beat fa-xl" style={{color:" #f0056b"}}></i></a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white mr-3" target="blank"href="https://www.linkedin.com/feed/"><i className="icon-social-linkedin"></i></a>
+                    <a className="social-link rounded-circle text-white mr-3" target="blank"href="https://www.linkedin.com/feed/"><i class="fa-brands fa-linkedin fa-beat fa-xl" style={{color:"#075ef2"}}></i></a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white" target="blank" href="https://github.com/Saponka"><i className="icon-social-github"></i></a>
+                    <a className="social-link rounded-circle text-white" target="blank" href="https://github.com/Saponka"><i class="fa-brands fa-github fa-xl fa-beat"   style={{color:"black"}}></i></a>
                 </li>
-            </ul>
-            <p id="foot" className=" small mb-0" > Franco Herrera Ruiz 2023</p>
+            </ul>}
+            <p id="foot" className=" small mb-0" > Franco Herrera Ruiz {date.getFullYear()}</p>
         </div>
     </footer>
     </>
