4826566d1847d47c9e4d4e6eaf5aea80150afaa8
Franco <francoherrera_8@hotmail.com.ar>
2023-06-12 00:50:51 -0300

    header aimate h1 , h2 , description , img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import perfil from "../../assets/Img/perfil.png";
 import "./header.css";
 
+
 const Header = () => {
   return (
     <>
       <div id="banner">
         <div className="divNombre" id="AcercaDeMi">
-          <h1 className="nombre">Franco Herrera Ruiz</h1>
-          <h2>
-            <em className="subtitulo">Programador Web FullStack</em>
+          <h1 className="nombre animate__animated animate__slideInLeft animate__delay-2s ">Franco Herrera Ruiz</h1>
+          <h2 className="animate__animated animate__lightSpeedInLeft animate__delay-4s animate__slow">
+            <em className="subtitulo ">Programador Web FullStack</em>
           </h2>
         </div>
         <div id="foto" style={{ display: "flex" }}>
-          <img id="imgfoto" src={perfil} alt=""></img>
+          <img id="imgfoto" src={perfil} alt="" className=" animate__animated animate__flipInY animate__delay-2s"></img>
         </div>
         <br />
       </div>
       <section className="content-section text-dark text-center  p-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div>
-            <h2 className="lead mb-5 descripcion">
+            <h2 className="lead mb-5 descripcion animate__animated animate__backInUp animate__delay-3s animate__slow">
               Soy Franco Herrera Ruiz de Río Cuarto, Córdoba, Argentina.
               <br />
               Actualmente soy Desarrollador Web FullStack y Chef Profesional.
