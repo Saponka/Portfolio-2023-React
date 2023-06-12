c5b0e0b13f6f630b98307089206374f3cd99eff0
Franco <francoherrera_8@hotmail.com.ar>
2023-06-11 23:50:52 -0300

    nav component function fecha y icons font awosome

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import "./navbar.css"
 //imports cv
 import resume from "../../assets/Data/FrancoResume.pdf";
 import qr from "../../assets/Img/FrancoResume.png"
 
 
 const NavBar = () => {
+
+  const fecha= new Date().getDate();
+  const año = new Date().getFullYear();
+function diasEnString (){
+  let dias =["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+  const day = new Date().getDay();
+  return dias[day]
+}
+function meses(){
+  let meses =["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+  const mes = new Date().getMonth();
+
+  return meses[mes]
+}
+
 
   return (
     <>
@@ -38,62 +54,30 @@ const NavBar = () => {
             <div className="offcanvas-body">
                 <ul className="navbar-nav justify-content-end  pe-3">
                     <li className="nav-item">
-                  {/* darkmode */}
-                  {/* <div id="capa">
-                    <div className="controls">
-                      <button className="ligth">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-sun"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                        </svg>
-                      </button>
-                      <button
-                        className="dark"
-                        style={{ backgroundColor: "black", color: "white" }}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-moon"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
-                        </svg>
-                      </button>
-                      <span> Dark Mode</span>
-                    </div>
-                  </div> */}
+                  <p className='fecha'>  <span>{diasEnString()}</span> <span>{fecha}</span> <span>{meses()}</span> <span>{año}</span></p>
                 </li>
                 <br/>
-                <div className="container px-4 px-lg-5 mb-2 social">
+                <div className="container px-4 px-lg-5 mb-1 social">
                     <a
                       className=" rounded-circle text-white "
                       target="blank"
                       href="https://www.instagram.com/frankoherreraruiz/?hl=es-la"
                     >
-                      <i className="icon-social-instagram"></i>
+                      <i class="fa-brands fa-instagram fa-beat-fade fa-2xl" style={{color:" #f0056b"}}></i>
                     </a>
                     <a
                       className="rounded-circle text-white "
                       target="blank"
                       href="https://www.linkedin.com/feed/"
                     >
-                      <i className="icon-social-linkedin"></i>
+                      <i class="fa-brands fa-linkedin fa-shake fa-2xl" style={{color:"#075ef2"}}></i>
                     </a>
                     <a
                       className=" rounded-circle text-white"
                       target="_blank"
                       href="https://github.com/Saponka"
                     >
-                      <i className="icon-social-github"></i>
+                      <i class="fa-brands fa-github fa-2xl fa-flip"  /* style={{color:"purple"}} */></i>
                     </a>
                 </div>
                  <br />
