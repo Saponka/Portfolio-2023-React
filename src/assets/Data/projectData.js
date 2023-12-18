a8d5fc07a3684cc6ad84f8eb4ff7e1c6e033b29f
Franco <francoherrera_8@hotmail.com.ar>
2023-12-18 20:55:20 -0300

    themes section

--- src/assets/Data/projectData.js
+++ src/assets/Data/projectData.js
@@ -1,66 +1,69 @@
 
 let itemData = {
 
+    games:[
+        {
+            id:1,
+            name: "A-Venture",
+            img:require("../Img/proyectos/a-venture.png"),
+            description:"Elige tu propia aventura y toma las decisiones",
+            url:"https://a-ventures.vercel.app/"
+        },
+        {
+            id:2,
+            name: "Color Game",
+            img:require("../Img/proyectos/colorete.png"),
+            description:"Juego de adivinar el color RGB (D.O.M)",
+            url:"https://color-game-blond.vercel.app/"
+        },
+        {
+            id:3,
+            name: "Typer 3000",
+            img:require("../Img/proyectos/typer3000.png"),
+            description:" Velocidad,Tiempo,Palabras Este Juego es para vos",
+            url:"https://typer3000.vercel.app/"
+        },
+      
+    ],
+
     items:[
-  /*   {
-        id:1,
-        name: "Weather Channel",
-        img:require('../Img/proyectos/weatherChannel.png'),
-        description:"Aplicacion del clima,donde se consume una api con Ajax",
-        url:"https://saponka.github.io/weather-channel/"
-    }, */
     {
-        id:2,
+        id:1,
         name: "Lista de Tareas",
         img:require("../Img/proyectos/ListaDeTareas.png"),
         description:"To Do List,para crear, editar y borrar(D.O.M)",
         url:"https://saponka.github.io/ListaTareas/"
     },
     {
-        id:3,
-        name: "A-Venture",
-        img:require("../Img/proyectos/a-venture.png"),
-        description:"Elige tu propia aventura y toma las decisiones",
-        url:"https://a-ventures.vercel.app/"
-    },
-    {
-        id:4,
-        name: "Color Game",
-        img:require("../Img/proyectos/colorete.png"),
-        description:"Juego de adivinar el color RGB (D.O.M)",
-        url:"https://color-game-blond.vercel.app/"
-    },
-    {
-        id:5,
+        id:2,
         name: "Comix Zoom",
         img:require("../Img/proyectos/comixZoom.png"),/* */
         description:"App de fandome de comics",
         url:"https://comixzoom.vercel.app/"
     },
     {
-        id:6,
+        id:3,
         name: "Resto Theme",
         img:require("../Img/proyectos/resto.png"),
         description:"Template para negocio gastronomico",
         url:"https://saponka.github.io/restaurante/"
     },
     {
-        id:7,
-        name: "Typer 3000",
-        img:require("../Img/proyectos/typer3000.png"),
-        description:" Velocidad,Tiempo,Palabras Este Juego es para vos",
-        url:"https://typer3000.vercel.app/"
+        id:4,
+        name: "Astronomy Theme",
+        img:require("../Img/proyectos/astronomy.png"),
+        description:"Template para Astronomia",
+        url:"https://astronomy-template.vercel.app/"
     },
-  
     {
-        id:8,
+        id:5,
         name: "Bonzai Tek ",
         img:require("../Img/proyectos/bonzayTek.png"),
         description:"Gestion de datos multiples(MERN stack)",
         url:"https://final-react-front.vercel.app/"
     },
     {
-        id:9,
+        id:6,
         name: "Vino Rojo Bodegon",
         img:require("../Img/proyectos/vinoRojo.png"),
         description:"E-commerce gastronomico (MERN stack)",
@@ -95,7 +98,7 @@ apis:[
     {
         id:4,
         name: "Movies API",
-        img:require("../Img/proyectos/videoGames.png"),
+        img:require("../Img/proyectos/Movies.png"),
         description:"API REST de Peliculas",
         url:"https://movie-api-gold-seven.vercel.app/"
     },
