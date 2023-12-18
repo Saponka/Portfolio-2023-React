c39ab2d894f101e6b975217d18fe150eead70fcd
Franco <francoherrera_8@hotmail.com.ar>
2023-12-18 18:28:02 -0300

    remove logos  animations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
         <div className="container px-4 px-lg-5">
             {<ul className="list-inline mb-5">
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white mr-3"target="blank" href="https://www.instagram.com/?hl=es-la"> <i class="fa-brands fa-instagram fa-beat fa-xl" style={{color:" #f0056b"}}></i></a>
+                    <a className="social-link rounded-circle text-white mr-3"target="blank" href="https://www.instagram.com/?hl=es-la"> <i class="fa-brands fa-instagram  fa-xl" style={{color:" #f0056b"}}></i></a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white mr-3" target="blank"href="https://www.linkedin.com/feed/"><i class="fa-brands fa-linkedin fa-beat fa-xl" style={{color:"#075ef2"}}></i></a>
+                    <a className="social-link rounded-circle text-white mr-3" target="blank"href="https://www.linkedin.com/feed/"><i class="fa-brands fa-linkedin fa-xl" style={{color:"#075ef2"}}></i></a>
                 </li>
                 <li className="list-inline-item">
-                    <a className="social-link rounded-circle text-white" target="blank" href="https://github.com/Saponka"><i class="fa-brands fa-github fa-xl fa-beat"   style={{color:"black"}}></i></a>
+                    <a className="social-link rounded-circle text-white" target="blank" href="https://github.com/Saponka"><i class="fa-brands fa-github fa-xl "   style={{color:"black"}}></i></a>
                 </li>
             </ul>}
             <p id="foot" className=" small mb-0" > Franco Herrera Ruiz {date.getFullYear()}</p>
