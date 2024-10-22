4ded07cfb8a91390eda4f3529b2ee04dc84568a8
Franco <francoherrera_8@hotmail.com.ar>
2024-10-22 16:43:05 -0300

    header animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,20 +8,20 @@ const Header = () => {
     <>
       <div id="banner">
         <div className="divNombre" id="AcercaDeMi">
-          <h1 className="nombre animate__animated animate__slideInLeft animate__delay-2s ">Franco Herrera Ruiz</h1>
-          <h2 className="animate__animated animate__lightSpeedInLeft animate__delay-4s animate__slow">
+          <h1 className="nombre animate__animated animate__slideInLeft animate__delay-1s ">Franco Herrera Ruiz</h1>
+          <h2 className="animate__animated animate__lightSpeedInLeft animate__delay-1s animate__slow">
             <em className="subtitulo ">Programador Web FullStack</em>
           </h2>
         </div>
         <div id="foto" style={{ display: "flex" }}>
-          <img id="imgfoto" src={perfil} alt="" className=" animate__animated animate__flipInY animate__delay-2s"></img>
+          <img id="imgfoto" src={perfil} alt="" className=" animate__animated animate__lightSpeedInLeft animate__delay-2s"></img>
         </div>
         <br />
       </div>
       <section className="content-section text-dark text-center  p-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div>
-            <h2 className="lead mb-5 descripcion animate__animated animate__backInUp animate__delay-3s animate__slow">
+            <h2 className="lead mb-5 descripcion ">
               Soy Franco Herrera Ruiz de Río Cuarto, Córdoba, Argentina.
               <br />
               Considero que siempre hay problemas y soluciones para aprender ante un
